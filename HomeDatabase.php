07d0a490a3d1da6187eb8242825f17758d83c618
Kirill Swerchkov <63578449+P1eas1ng@users.noreply.github.com>
2020-05-21 17:25:02 +0300

    Update HomeDatabase.php

--- HomeDatabase.php
+++ HomeDatabase.php
@@ -3,7 +3,38 @@
 <link href="https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet"></link>
  <title>Home Database</title>
  <style>
+     body{
+          margin:0px;
+        }
+        .menu-item a:hover{
+          color:black;
+        }
+        .menu-item a{
+          text-decoration: none;
+          color:#ffa100;
+          font-size:19px;
+        }
+        @media screen and (max-width: 600px) {
+          .header{
+            justify-content:center;
+            text-align:center;
+            flex-direction:column !important;
+          }
+        }
+        .header{
+        box-shadow: 0 0 12px #bbb;
+        display: flex;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        width: 100%;
+        justify-content: space-around;
+        background: white;
+        }
         .container {
+            margin-top:70px;
+            display:flex;
+            flex-direction: column;
+            align-items: center;
             max-width: 700px;
         }
  
@@ -60,7 +91,7 @@
       firebase.initializeApp(firebaseConfig);
       var database = firebase.database();
       function writeUserData(link, about, how, alias) {
-        firebase.database().ref('database/' + alias).set({
+        firebase.database().ref('database/' + 'home/' + alias).set({
           about: about,
           link: link, 
           how : how,
@@ -83,10 +114,67 @@
            <input id="link-1" class="input" placeholder="Link"><br>
            <input id="alias-1" class="input" placeholder="Alias"><br>
            <button onclick="send();">Send link</button>
+           <script>
+            function send(){
+                var ali1 = document.getElementById('alias-1').value;
+               if  (ali1 !== ''){
+               var about1 = document.getElementById('about-1').value;
+               var how1 = document.getElementById('how-1').value;
+               var link1 = document.getElementById('link-1').value;
+               var alias1 = document.getElementById('alias-1').value;
+                writeUserData(link1, about1, how1, alias1);
+                retr();
+               }else{
+               retr();
+               }
+            }
+           </script>
         </div>
-         <div id="container">
+         <div id="showing">
             
          </div>
+         <style>
+            #showing{
+                display:flex;
+                height:150px;
+                overflow:scroll;
+                flex-direction:column;
+                align-items:center;
+            }
+             .full-scape{
+                 display:flex;
+                 flex-direction:column;
+             }
+             .full-scape:hover{
+                background:rgb(211, 208, 208);
+             }
+         </style>
         </div>
+        <script>
+            retr();
+            function retr(){
+                document.getElementById("showing").innerHTML = '';
+                var query = firebase.database().ref("database/" + 'home/');
+                query.once("value")
+                .then(function(snapshot) {
+                    snapshot.forEach(function(childSnapshot) {
+                    var elem = document.createElement("div");
+                    elem.style="display:flex;flex-direction:column;margin-top:40px;";
+                    var pol = '<div class="full-scape">';
+                    var about = childSnapshot.val().about;
+                    pol = pol + '<div class="about">About: ' + about + '</div>';
+                    var how = childSnapshot.val().how;
+                    pol = pol + '<div class="how">Addon: ' + how + '</div>';
+                    var link = childSnapshot.val().link;
+                    pol = pol + '<div class="link">Link: <a href="' + link + '">'+ link + '</a></div>';
+                    var alias = childSnapshot.val().alias;
+                    pol = pol + '<div class="alias">Alias: '+ alias +'</div></div>';
+                    console.log(pol);
+                    elem.innerHTML = pol;
+                    document.getElementById("showing").appendChild(elem);
+                    });
+                });
+            }
+        </script>
 </body>
 </html>
